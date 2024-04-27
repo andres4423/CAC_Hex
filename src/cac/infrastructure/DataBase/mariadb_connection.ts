b0b235aa28ee8dd2290e3dfdf1baea9e6741b325
andres4423 <olaya4423@gmail.com>
2024-04-27 16:00:45 -0500

    intento bd

--- src/cac/infrastructure/DataBase/mariadb_connection.ts
+++ src/cac/infrastructure/DataBase/mariadb_connection.ts
@@ -1,32 +1,30 @@
-import { Connection, createConnection } from "mariadb";
-import { Cita } from "../../domain/model/cita/cita";
-
+import { Connection,  createPool } from "mariadb";
+require('dotenv').config();
 export class MariaDBConnector {
-  private connection: Connection | null = null;
+  private connectionPool: any;
 
-  async connect(): Promise<void> {
-    if (!this.connection) {
-      this.connection = await createConnection({
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-      });
-    }
+  constructor() {
+    console.log(process.env.DB_PORT)
+    this.connectionPool = createPool({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
   }
 
   async getConnection(): Promise<Connection> {
-    if (!this.connection) {
-      await this.connect();
+    try {
+      const connection = await this.connectionPool.getConnection();
+      return connection;
+    } catch (error) {
+      console.error("Error getting connection from pool:", error);
+      throw error; // Re-throw the error for further handling
     }
-    return this.connection;
   }
 
   async close(): Promise<void> {
-    if (this.connection) {
-      await this.connection.end();
-      this.connection = null;
-    }
+    await this.connectionPool.end();
   }
 }
