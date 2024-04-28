ff8d4b09bae0e0d472321ff0f9cb04763e7053c3
andres4423 <olaya4423@gmail.com>
2024-04-28 18:25:05 -0500

    tests

--- test/cac/util/testCitaCRUD.test.ts
+++ test/cac/util/testCitaCRUD.test.ts
@@ -12,7 +12,7 @@ describe('MariaDBCitaRepository', () => {
     afterAll(async () => {
         await connection.close();
       });
-    it('debería crear una cita correctamente y devolver la cita creada', async () => {
+    it('Crear cita', async () => {
         // Arrange: Simular la creación correcta de una cita
         const citaMock: Cita = new Cita(
             1, 
@@ -33,6 +33,7 @@ describe('MariaDBCitaRepository', () => {
 
         // Assert: Verificar que devuelve la cita creada correctamente
         expect(result).toEqual(citaMock);
+        expect(result.id).toEqual(citaMock.id);
     });
 
     
@@ -58,5 +59,79 @@ describe('MariaDBCitaRepository', () => {
         expect(result).toEqual(citaMock);
         expect(citaRepository.actualizarCita).toHaveBeenCalledWith(citaMock);
         expect(result).not.toBeUndefined();
+        expect(result.id).toEqual(citaMock.id);
     });
+
+    it('Eliminar Cita', async () => {
+        // Arrange: Simular la creación correcta de una cita
+        const citaMock: Cita = new Cita(
+            1, 
+            'andrew',
+            'Calle 1223', 
+            '11:00', 
+            'Consulta odontologica', 
+            new Date('2024-05-09'), 
+            1234567890, 
+            1, 
+            0, 
+            'Hospital 1'  
+        );
+        jest.spyOn(citaRepository, 'eliminarCita').mockResolvedValue(citaMock);
+
+        const result = await citaRepository.eliminarCita(1);
+
+        expect(result).toBeDefined();
+        expect(result).toEqual(citaMock);
+        expect(citaRepository.actualizarCita).toHaveBeenCalledWith(citaMock);
+        expect(result).not.toBeUndefined();
+    });
+    it('Obtener cita por id', async () => {
+        // Arrange: Simular la creación correcta de una cita
+        const citaMock: Cita = new Cita(
+            1, 
+            'andrew',
+            'Calle 1223', 
+            '11:00', 
+            'Consulta odontologica', 
+            new Date('2024-05-09'), 
+            1234567890, 
+            1, 
+            0, 
+            'Hospital 1'  
+        );
+        jest.spyOn(citaRepository, 'getCitaById').mockResolvedValue(citaMock);
+
+        const result = await citaRepository.getCitaById(1);
+
+        expect(result).toBeDefined();
+        expect(result).toEqual(citaMock);
+        expect(citaRepository.actualizarCita).toHaveBeenCalledWith(citaMock);
+        expect(result).not.toBeUndefined();
+    });
+    
+    it('Obtener citas', async () => {
+        // Arrange: Simular la creación correcta de una cita
+        const citaMock: Cita[] =
+        [ new Cita(
+            1, 
+            'andrew',
+            'Calle 1223', 
+            '11:00', 
+            'Consulta odontologica', 
+            new Date('2024-05-09'), 
+            1234567890, 
+            1, 
+            0, 
+            'Hospital 1'  
+        )
+    ]
+        jest.spyOn(citaRepository, 'getCitas').mockResolvedValue(citaMock);
+
+        const result = await citaRepository.getCitas();
+
+        expect(result).toBeDefined();
+        expect(result).toEqual(citaMock);
+        expect(result).not.toBeUndefined();
+    });
+    
 });
