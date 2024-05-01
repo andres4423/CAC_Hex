92984f0d6cb05126396d131cc80767e6d0fa9ed9
andres4423 <olaya4423@gmail.com>
2024-04-30 19:11:56 -0500

    .

---
+++ src/cac/domain/port/driver/citaUseCasePort.ts
@@ -0,0 +1,11 @@
+import { Admin } from "../../model/admin/admin";
+import { Cita } from "../../model/cita/cita";
+
+export interface CitaServicePort {
+    getCitas(): Promise<Cita[]>;
+    getCitaById(id: number): Promise<Cita | null>;
+    crearCita(cita: Cita): Promise<Cita>;
+    actualizarCita(cita: Cita): Promise<Cita>;
+    eliminarCita(id: number): Promise<Cita | null>;
+    ingresarComoAdmin(id: number, password:string):Promise<Admin | null>
+  }
