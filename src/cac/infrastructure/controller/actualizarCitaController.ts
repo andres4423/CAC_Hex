fd7906355fd71ddb5f0916761f77a96962d9a907
andres4423 <olaya4423@gmail.com>
2024-05-03 09:54:36 -0500

    .

--- src/cac/infrastructure/controller/actualizarCitaController.ts
+++ src/cac/infrastructure/controller/actualizarCitaController.ts
@@ -1,31 +1,26 @@
 import { Request, Response } from 'express';
 import { actualizarCitaUseCase } from '../../application/usecase/actualizarCitaUseCase';
 import { ObtenerCitaPorIdUseCase } from '../../application/usecase/ObtenerCitaID';
-import { Cita } from '../../domain/model/cita/cita';
-
-export default class actualizarCita {
-    private citaUseCase: actualizarCitaUseCase;
-    private getCitaID: ObtenerCitaPorIdUseCase;
-
+export default class actualizarCita{
+    private citaUseCase: actualizarCitaUseCase; 
+    private getCitaID: ObtenerCitaPorIdUseCase
     constructor(citaUseCase: actualizarCitaUseCase, getCitaID: ObtenerCitaPorIdUseCase) {
-        this.citaUseCase = citaUseCase;
-        this.getCitaID = getCitaID;
+        this.citaUseCase = citaUseCase; 
+        this.getCitaID = getCitaID
     }
-
+      
     updateCita = async (req: Request, res: Response): Promise<void> => {
         const citaId = parseInt(req.params.id);
-
+        
         try {
-            // Obtener la cita completa usando su ID
-            const citaActualizada = await this.getCitaID.getCitaById(citaId);
-
-            // Verificar si la cita existe
-            if (citaActualizada === null) {
+            const citaActualizada = await this.getCitaID.execute(citaId);
+            
+        
+            if (!citaActualizada) {
                 res.status(404).json({ error: `Cita con ID ${citaId} no encontrada` });
                 return;
             }
-
-            // Verificar si las propiedades existen en req.body antes de asignarlas
+            
             if (req.body.fecha) {
                 citaActualizada.setFecha(req.body.fecha);
             }
@@ -35,14 +30,14 @@ export default class actualizarCita {
             if (req.body.lugar) {
                 citaActualizada.setLugar(req.body.lugar);
             }
-
-            // Llamar a la función actualizarCita con la cita actualizada
+    
             const updatedCita = await this.citaUseCase.execute(citaActualizada);
-
+            
             res.status(200).json(updatedCita);
         } catch (error) {
             console.error(`Error updating cita with ID ${citaId}:`, error);
             res.status(500).json({ error: 'Internal server error' });
         }
     };
+
 }
