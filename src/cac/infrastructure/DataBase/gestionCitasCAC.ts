efa835ade0d478bd855309bed6881c15ba7e7f0d
andres4423 <olaya4423@gmail.com>
2024-05-12 09:24:27 -0500

    .

--- src/cac/infrastructure/DataBase/gestionCitasCAC.ts
+++ src/cac/infrastructure/DataBase/gestionCitasCAC.ts
@@ -1,3 +1,4 @@
+
 import { Admin } from "../../domain/model/admin/admin";
 import { Banco } from "../../domain/model/bancos/banco";
 import { Cita } from "../../domain/model/cita/cita";
@@ -8,12 +9,14 @@ export class gestionCitas implements CitaRepositoryPort {
   private readonly dbConnector: MySQLConnector;
 
   constructor() {
+    // Inicialización del conector a la base de datos MySQL
     this.dbConnector = new MySQLConnector();
     this.dbConnector.connect(); // Establecer conexión al crear una instancia de GestionCitas
   }
 
   async getCitas(): Promise<Cita[]> {
     try {
+      // Consulta para obtener todas las citas desde la base de datos
       const results = await this.dbConnector.query("SELECT * FROM registro_citas");
       return results;
     } catch (error) {
@@ -24,6 +27,7 @@ export class gestionCitas implements CitaRepositoryPort {
 
   async getCitaById(id: number): Promise<Cita | null> {
     try {
+      // Consulta para obtener una cita por su ID desde la base de datos
       const results = await this.dbConnector.query("SELECT * FROM registro_citas WHERE id = ?", [id]);
       return results[0] || null; // Devolver la primera cita encontrada o null si no hay resultados
     } catch (error) {
@@ -34,8 +38,9 @@ export class gestionCitas implements CitaRepositoryPort {
 
   async crearCita(cita: Cita): Promise<Cita> {
     try {
+      // Consulta para insertar una nueva cita en la base de datos
       await this.dbConnector.query(
-        "INSERT INTO registro_citas (id, nombre, direccion, hora, descripcion, fecha, celular, tipo_cita, premium, lugar, fecha_nacimiento) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ? ?)",
+        "INSERT INTO registro_citas (id, nombre, direccion, hora, descripcion, fecha, celular, tipo_cita, premium, lugar, fecha_nacimiento) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
         [cita.id, cita.nombre, cita.direccion, cita.hora, cita.descripcion, cita.fecha, cita.celular, cita.tipo_cita, cita.premium, cita.lugar, cita.fecha_nacimiento]
       );
       return cita;
@@ -47,6 +52,7 @@ export class gestionCitas implements CitaRepositoryPort {
 
   async actualizarCita(cita: Cita): Promise<Cita> {
     try {
+      // Consulta para actualizar una cita existente en la base de datos
       await this.dbConnector.query(
         "UPDATE registro_citas SET hora = ?, fecha = ?, lugar = ? WHERE id = ?",
         [cita.hora, cita.fecha, cita.lugar, cita.id]
@@ -60,6 +66,7 @@ export class gestionCitas implements CitaRepositoryPort {
 
   async eliminarCita(id: number): Promise<Cita | null> {
     try {
+      // Consulta para eliminar una cita por su ID de la base de datos
       await this.dbConnector.query("DELETE FROM registro_citas WHERE id = ?", [id]);
       return null;
     } catch (error) {
@@ -70,16 +77,16 @@ export class gestionCitas implements CitaRepositoryPort {
 
   async ingresarComoAdmin(id: number, password: string): Promise<Admin | null> {
     try {
+        // Consulta para verificar las credenciales de administrador en la base de datos
         const results = await this.dbConnector.query("SELECT * FROM admin WHERE id = ? AND contraseña = ?", [id, password]);
       
         if (results.length > 0) {
-          
+          // Si se encuentra un administrador con las credenciales proporcionadas, se crea un objeto Admin
             const adminData = results[0];
-           
             const admin = new Admin(adminData.id, adminData.contraseña);
             return admin;
         } else {
-
+            // Si no se encuentra un administrador con las credenciales proporcionadas, se devuelve null
             return null;
         }
     } catch (error) {
@@ -90,16 +97,16 @@ export class gestionCitas implements CitaRepositoryPort {
 
 async ingresarBanco(id: number, password: string): Promise<Banco | null> {
   try {
+      // Consulta para verificar las credenciales de un banco en la base de datos
       const results = await this.dbConnector.query("SELECT * FROM BANCO_USER WHERE id = ? AND contraseña = ?", [id, password]);
     
       if (results.length > 0) {
-        
+        // Si se encuentra un banco con las credenciales proporcionadas, se crea un objeto Banco
           const bancoData = results[0];
-         
           const banco = new Banco(bancoData.id, bancoData.contraseña);
           return banco;
       } else {
-
+          // Si no se encuentra un banco con las credenciales proporcionadas, se devuelve null
           return null;
       }
   } catch (error) {
--- src/cac/infrastructure/controller/citasController.ts
+++ src/cac/infrastructure/controller/citasController.ts
@@ -10,17 +10,18 @@ export default class CitasController {
         this.citaUseCase = citaUseCase; 
     }
 
+    //obtener todas las citas
     getAllCitas = async (_req: Request, res: Response): Promise<void> => {
+        // Se hace el manejo de errores con try catch
         try {
+            // se llama al metodo de obtener todas las citas
             const citas = await this.citaUseCase.getCitas();
+            // Devuelve las citas en formato json
             res.status(200).json(citas);
         } catch (error) {
+            // envia mensaje de error en caso de que no se puedan devolver las citas.
             console.error('Error retrieving citas:', error);
             res.status(500).json({ error: 'Internal server error' });
         }
     };
-
-    ;
-
-
 }
